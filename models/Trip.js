61a12d35b002e56a324de45f52625e62d0a5bf0c
Abhijit Ranjan <142292073+abhijit826@users.noreply.github.com>
2025-03-02 07:59:40 +0530

    Updated Trip.js

--- models/Trip.js
+++ models/Trip.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose');
 
+const tripSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  budget: {
+    type: String,
+    required: true,
+  },
+  companions: {
+    type: String,
+    required: true,
+  },
+  activities: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
